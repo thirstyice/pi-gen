364c83b778dcf31b8227f539c0ed65275ba580c3
thirstyice <tauranwood@gmail.com>
2021-05-19 15:47:16 -0600

    Added volumeicon package

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -11,6 +11,7 @@ install -v -m 644 files/panel/datetime-7.rc	"${ROOTFS_DIR}/etc/xdg/xfce4/panel/"
 install -v -m 644 files/panel/whiskermenu-1.rc	"${ROOTFS_DIR}/etc/xdg/xfce4/panel/"
 install -v -m 644 files/defaults.list		"${ROOTFS_DIR}/etc/xfce4/"
 install -v -m 644 files/defaults		"${ROOTFS_DIR}/usr/share/xfwm4/"
+install -v -m 644 files/autostart/volumeicon.desktop	"${ROOTFS_DIR}/etc/xdg/autostart/"
 
 
 install -v -m 644 -d files/Tela-circle		"${ROOTFS_DIR}/usr/share/icons/"
@@ -22,3 +23,9 @@ install -v -m 644 files/01_thirstyOS.conf	"${ROOTFS_DIR}/usr/share/lightdm/light
 install -v -m 644 files/lxterminal.conf		"${ROOTFS_DIR}/usr/share/lxterminal/"
 
 install -v -m 644 files/com.ubuntu.pkexec.thunar.policy.conf	"${ROOTFS_DIR}/usr/share/polkit-1/actions/"
+
+install -v -m 644 files/volumeicon-alsa_0.5.1+git20171004-thirstyice_armv7l.deb\
+						"${ROOTFS_DIR}/var/cache/apt/archives/"
+on_chroot << EOF
+dpkg -i /var/cache/apt/archives/volumeicon-alsa_0.5.1+git20171004-thirstyice_armv7l.deb
+EOF
