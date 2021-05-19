ad326b737c4704762e7be5e5262bf3fc61b52614
thirstyice <tauranwood@gmail.com>
2021-05-19 16:40:10 -0600

    Customize os-release

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -3,3 +3,17 @@
 if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
 	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
 fi
+
+echo "PRETTY_NAME=\"${IMG_NAME} ${IMG_VERSION} ${RELEASE}\"
+NAME=\"${IMG_NAME}\"
+VERSION_ID=\"${VERSION}\"
+VERSION=\"${VERSION} (${RELEASE})\"
+VERSION_CODENAME=${RELEASE}
+ID=${IMG_NAME}
+ID_LIKE=debian
+HOME_URL=\"http://www.raspbian.org/\"
+SUPPORT_URL=\"http://www.raspbian.org/RaspbianForums\"
+BUG_REPORT_URL=\"http://www.raspbian.org/RaspbianBugs\"
+" > files/os-release
+
+install -v -m 644 files/os-release "${ROOTFS_DIR}/usr/lib/"
