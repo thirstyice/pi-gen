6c3c0a64a1c9feafb30534b6060d6b79504c9c50
thirstyice <tauranwood@gmail.com>
2021-05-19 00:32:55 -0600

    Made geany the default editor

--- stage4/00-install-packages/01-run.sh
+++ stage4/00-install-packages/01-run.sh
@@ -5,4 +5,6 @@ update-alternatives --install /usr/bin/x-www-browser \
   x-www-browser /usr/bin/firefox 86
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/firefox 86
+update-alternatives --install /usr/bin/editor \
+  editor /usr/bin/geany 86
 EOF
