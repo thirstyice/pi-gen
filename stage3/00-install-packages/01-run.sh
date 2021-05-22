8c3c739aa42099562bc8218f9bc1c2da6b4b4539
thirstyice <tauranwood@gmail.com>
2021-05-22 13:10:06 -0600

    Add missing config files

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -7,6 +7,8 @@ install -v -m 644 files/keyboards.xml		"${ROOTFS_DIR}/etc/xdg/xfce4/xfconf/xfce-
 install -v -m 644 files/xfce4-desktop.xml	"${ROOTFS_DIR}/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/"
 install -v -m 644 files/xfce4-panel.xml		"${ROOTFS_DIR}/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/"
 ln -sfn ../xfconf/xfce-perchannel-xml/xfce4-panel.xml "${ROOTFS_DIR}/etc/xdg/xfce4/panel/default.xml"
+install -v -m 644 files/xfwm4.xml		"${ROOTFS_DIR}/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/"
+install -v -m 644 files/xsettings.xml		"${ROOTFS_DIR}/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/"
 
 install -v -m 644 files/panel/datetime-7.rc	"${ROOTFS_DIR}/etc/xdg/xfce4/panel/"
 install -v -m 644 files/panel/whiskermenu-1.rc	"${ROOTFS_DIR}/etc/xdg/xfce4/panel/"
@@ -16,11 +18,11 @@ install -v -m 644 files/volumeicon.desktop	"${ROOTFS_DIR}/etc/xdg/autostart/"
 
 
 cp -v -r files/Tela-circle			"${ROOTFS_DIR}/usr/share/icons/"
-chmod -v -R 644 "${ROOTFS_DIR}/usr/share/icons/Tela-circle/"
+chmod -v -R 755 "${ROOTFS_DIR}/usr/share/icons/Tela-circle/"
 cp -r -v files/Tela-circle-dark			"${ROOTFS_DIR}/usr/share/icons/"
-chmod -v -R 644 "${ROOTFS_DIR}/usr/share/icons/Tela-circle-dark/"
+chmod -v -R 755 "${ROOTFS_DIR}/usr/share/icons/Tela-circle-dark/"
 cp -v -r files/WhiteSur-dark-solid		"${ROOTFS_DIR}/usr/share/themes/"
-chmod -v -R 644 "${ROOTFS_DIR}/usr/share/themes/WhiteSur-dark-solid/"
+chmod -v -R 755 "${ROOTFS_DIR}/usr/share/themes/WhiteSur-dark-solid/"
 
 install -v -m 644 files/lightdm-gtk-greeter.conf "${ROOTFS_DIR}/etc/lightdm/"
 install -v -m 644 files/login-background.svg	"${ROOTFS_DIR}/usr/share/lightdm/"
